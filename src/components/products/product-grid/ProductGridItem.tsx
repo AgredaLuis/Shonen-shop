7b76488e6ba7a72cc0ad2733625950a9cb8a527a
Luis Agreda <luis.agreda98@gmail.com>
2025-04-21 02:16:56 -0400

    Fix Images to Show on ProductGridItem

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,45 +1,50 @@
-'use client';
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
 
-import Image from 'next/image';
-import Link from 'next/link';
-
-import { Product } from '@/interfaces';
-import { useState } from 'react';
+import { Product } from "@/interfaces";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
+export const ProductGridItem = ({ product }: Props) => {
+  const localSrc1 = product?.images[0]
+    ? product?.images[0].startsWith("http") // https://urlcompletodelaimagen.jpg
+      ? product?.images[0]
+      : `/products/${product?.images[0]}`
+    : "/imgs/placeholder.jpg";
 
-export const ProductGridItem = ( { product }: Props ) => {
+  const localSrc2 = product?.images[1]
+    ? product?.images[1].startsWith("http") // https://urlcompletodelaimagen.jpg
+      ? product?.images[1]
+      : `/products/${product?.images[1]}`
+    : "/imgs/placeholder.jpg";
 
-  const [ displayImage, setDisplayImage ] = useState( product.images[ 0 ] );
-
+  const [displayImage, setDisplayImage] = useState(localSrc1);
 
   return (
     <div className="rounded-md overflow-hidden fade-in ">
-      <Link href={ `/product/${ product.slug }` }>
+      <Link href={`/product/${product.slug}`}>
         <Image
-          src={ `/products/${ displayImage }` }
-          alt={ product.title }
+          src={`/products/${displayImage}`}
+          alt={product.title}
           className="w-full object-cover rounded"
-          width={ 500 }
-          height={ 500 }
-          onMouseEnter={ () => setDisplayImage( product.images[1] )  }
-          onMouseLeave={ () => setDisplayImage( product.images[0] ) }
+          width={500}
+          height={500}
+          onMouseEnter={() => setDisplayImage(localSrc2)}
+          onMouseLeave={() => setDisplayImage(localSrc1)}
         />
       </Link>
 
       <div className="p-4 flex flex-col">
-        <Link
-          className="hover:text-blue-600"
-          href={ `/product/${ product.slug }` }>
-          { product.title }
+        <Link className="hover:text-blue-600" href={`/product/${product.slug}`}>
+          {product.title}
         </Link>
-        <span className="font-bold">${ product.price }</span>
+        <span className="font-bold">${product.price}</span>
       </div>
-
     </div>
   );
 };
